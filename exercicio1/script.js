5b6ca07dd5f5390828b44b7a957d4d518f54a669
Bendinelle <bendinellework@gmail.com>
2023-02-16 23:23:15 -0300

    objetos-exerc

---
+++ exercicio1/script.js
@@ -0,0 +1,15 @@
+
+const sacolao = [];
+
+
+const manga = {nome: 'manga', preco: 2.5, disponibilidade: true};
+const mixirica = {nome: 'mixirica', preco: 4.0, disponibilidade: false};
+const lichia = {nome: 'lichia', preco: 1.8, disponibilidade: true};
+
+sacolao.push(manga)
+sacolao.push(mixirica)
+sacolao.push(lichia)
+
+console.log(sacolao)
+
+
